6a59a26dd44aaa033b399da8404f58bb14dc8b1f
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2022-01-18 10:47:23 +0000

    Added github repository link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      Weather
+      <footer>
+        <a
+          href="https://github.com/RitaCavalheiro/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourse code{" "}
+        </a>{" "}
+        by Rita Cavalheiro{" "}
+      </footer>
+    </div>
+  );
 }
